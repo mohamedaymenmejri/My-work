75afdf464526912071dcb55e5bd8716a663752e7
Mohamed Aymen Mejri <medaymen.mejri@laposte.net>
2014-09-30 11:04:23 +0100

    Initial project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labex::Application.config.secret_key_base = 'f2f5aed901dedf425c3c400fedecc40fe42773d4f797c9ad5a010979868089ae630bd7334b8e7c834d286e739e0a189bcd6aa58641eb3280743f6a0df1bb2e1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labex::Application.config.session_store :cookie_store, key: '_labex_session'
