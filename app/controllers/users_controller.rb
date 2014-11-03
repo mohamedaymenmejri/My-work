0c45be8ea940bcd32989c5b3bd89e2bc7d836d31
Mohamed Aymen Mejri <medaymen.mejri@laposte.net>
2014-11-03 21:14:58 +0000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
         @user = User.new(secure_params)
         if @user.save
            # Handle a successful save.
-           flash[:success] = "Welcome to the Twitter App!"
-           redirect_to @user
+            sign_in @user       #  NEW LINE
+            flash[:success] = "Welcome to the Sample App!"    # NEW LINE
+            redirect_to @user   # NEW LINE
         else
             # Handle an unsuccessful save.   
             render 'new'  
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@
       validates :password, presence: true, length: { minimum: 6 }
       validates :password_confirmation, presence: true
       has_secure_password
+      before_save do |user| 
+              user.email = email.downcase 
+            user.remember_token = SecureRandom.urlsafe_base64
+            end
     end  
     
