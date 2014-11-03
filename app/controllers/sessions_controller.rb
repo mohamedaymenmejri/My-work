54c26457f3652fc2b23cac888134888c608ffd85
Mohamed Aymen Mejri <medaymen.mejri@laposte.net>
2014-11-03 20:26:23 +0000

    Finish user signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+      def new
+      end
+
+      def destroy
+      end
+
+      def create
+        user = User.find_by_email(params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+        	redirect_to user
+          # Sign the user in and redirect to the user's show page.
+        else
+          flash.now[:error] = 'Invalid email/password combination'
+          render 'new'
+        end
+     end
+end
